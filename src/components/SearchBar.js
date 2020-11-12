0984e271c9bbbb39d6a7615c9a4050c907ddbae4
peach-bit <hodanahmed00@gmail.com>
2020-11-12 16:31:34 +0000

    can map over videos and set state for each one

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,25 +3,37 @@ import youtube from '../api/youtubeAPI';
 
 export default class SearchBar extends React.Component {
 	constructor(props) {
-		super(props)
-		this.state = { input: "" }
+		super(props);
+		this.state = { input: "", videoList: [], video: "", videoLink: "" }
 	}
-	//Passing event object because it's necessary to use preventDefault function
-	// Asyn await is necessary to make GET requests to Youtube API
-	onInputSubmit = async(e) => {
+
+		//Passing event object because it's necessary to use preventDefault function
+		// Asyn await is necessary to make GET requests to Youtube API
+	componentDidUpdate () {
+	onInputSubmit = async (e) => {
 		e.preventDefault()
-		try {
-			const response = await youtube.get('/search', {
-				params: {
-					q: `${this.state.input}`
-				}
-			})
-			console.log(response)
-		} catch (e) {
-			throw e
-		} 
+		const response = await youtube.get('/search', {
+			params: {
+				q: `${this.state.input}`
+			}
+		})
+		// Add the response data to videoList state
+		this.setState({ videoList: response.data.items })
 	}
+	mapOverVideoList = () => {
+		const videolist = this.state.videoList
+		videolist.map((video) => {
+			const videoLink = `youtube.com/watch?v=${video.id.videoId}`
+			this.setState({ video })
+			this.setState({ videoLink: videoLink })
+			console.log(this.state.video + this.state.videoLink)
+			return <video src={this.state.videoLink}></video>
+		})
+	}
+	}
+
 	render() {	
+
 		return (
 			<div className="ui segment">
 				<div className="ui form">
@@ -38,6 +50,7 @@ export default class SearchBar extends React.Component {
 						</form>
 					</div>
 				</div>
+				{this.mapOverVideoList()}
 			</div>
 		)
 	}
